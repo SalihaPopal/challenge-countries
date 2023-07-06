8f951e4496b0c795e5410a9141fea3ecbf9bc984
Saliha Popal <salihapopal@gmail.com>
2023-07-06 14:38:47 +0100

    Level 2
    Added a search bar.

--- src/CountriesData.jsx
+++ src/CountriesData.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import data from "./countriesAll";
 
 
 const CountriesData = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredList, setFilteredList] = useState([]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  useEffect(() => {
+     const filtered = data.filter((country) => (
+  country.name.toLowerCase().includes(searchTerm.toLowerCase())
+     ));
+     setFilteredList(filtered);
+  }, [searchTerm])
+
   return (
     <div>
       <div className='header'>
@@ -13,8 +27,14 @@ const CountriesData = () => {
       </div>
       </div>
       <div className='cart-container'>
+        <input className='input'
+          type="search"
+          placeholder='Search for a country...'
+          onChange={handleSearch}
+          value={searchTerm}
+        />
         
-      {data.map((country, index) =>(
+      {filteredList.map((country, index) =>(
         <div key={index} className='country-card'> 
           <img
             src={`https://flagcdn.com/${country.alpha2Code.toLowerCase()}.svg`}
